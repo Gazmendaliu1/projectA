2b0fe4fc6bbd3758f5eb4506a89fa0ff1f0d263d
Gazmend Aliu <gazmend.aa@gmail.com>
2023-07-03 12:46:08 +0200

    ProjectA go

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsComponent } from './news/news.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  
+  path: "news" ,component : NewsComponent
+}, {
+  path:"about",component : AboutComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
