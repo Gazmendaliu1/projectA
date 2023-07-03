c54ba8a42516aed6aa9f95a6bb0c49d7b3033706
Georgios Tsoulakis <georgtsou9@gmail.com>
2023-07-03 15:38:44 +0200

    fixed courses

---
+++ src/app/Icourses.ts
@@ -0,0 +1,7 @@
+export interface Icourses{
+    name:string,
+    price:number,
+    image:string,
+    duration:string,
+    level:string
+}
--- src/app/courses.ts
+++ src/app/courses.ts
@@ -1,44 +1,44 @@
 export const courses = [
-    {
-        name:"Front End Developer Course",
-        price:2990,
-        image:""  ,
-        duration:"8 weeks",
-        level:"Beginner and Advanced"
-     },
-     {
-        name:"Back End Developer Course",
-        price:2990,
-        image:""  ,
-        duration:"7 weeks",
-        level:"Beginner and Advanced"
-     },
-     {
-        name:"Full Stack Web Developer Course",
-        price:4490,
-        image:""  ,
-        duration:"15 weeks",
-        level:"Beginner and Advanced"
-     },
-     {
-        name:"Basic Course JavaScript",
-        price:290,
-        image:""  ,
-        duration:"2 days",
-        level:"Beginner"
-     },
-     {
-        name:"Basic Course HTML/CSS",
-        price:590,
-        image:""  ,
-        duration:"2 days",
-        level:"Beginner"
-     },
-     {
-        name:"Python Programming Course",
-        price:590,
-        image:""  ,
-        duration:"5 days",
-        level:"Beginner"
-     },
+   {
+   name:"Front End Developer Course",
+   price:2990,
+   image:"./assets/cardimages/frontend.webp"  ,
+   duration:"8 weeks",
+   level:"Beginner and Advanced"
+},
+{
+   name:"Back End Developer Course",
+   price:2990,
+   image:"./assets/cardimages/backend.jpeg"  ,
+   duration:"7 weeks",
+   level:"Beginner and Advanced"
+},
+{
+   name:"Full Stack Web Developer Course",
+   price:4490,
+   image:"./assets/cardimages/fullstack.jpeg"  ,
+   duration:"15 weeks",
+   level:"Beginner and Advanced"
+},
+{
+   name:"Basic Course JavaScript",
+   price:290,
+   image:"./assets/cardimages/js.png"  ,
+   duration:"2 days",
+   level:"Beginner"
+},
+{
+   name:"Basic Course HTML/CSS",
+   price:590,
+   image:"./assets/cardimages/html.jpg",
+   duration:"2 days",
+   level:"Beginner"
+},
+{
+   name:"Python Programming Course",
+   price:590,
+   image:"./assets/cardimages/python.png" ,
+   duration:"5 days",
+   level:"Beginner"
+}
 ]
