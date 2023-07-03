a71fb9c745d57f3a40235c704967d783432f2e99
Georgios Tsoulakis <georgtsou9@gmail.com>
2023-07-03 12:17:58 +0200

    cards object array and inteface

---
+++ src/app/courses.ts
@@ -0,0 +1,44 @@
+export const courses = [
+    {
+        name:"Front End Developer Course",
+        price:2990,
+        image:""  ,
+        duration:"8 weeks",
+        level:"Beginner and Advanced"
+     },
+     {
+        name:"Back End Developer Course",
+        price:2990,
+        image:""  ,
+        duration:"7 weeks",
+        level:"Beginner and Advanced"
+     },
+     {
+        name:"Full Stack Web Developer Course",
+        price:4490,
+        image:""  ,
+        duration:"15 weeks",
+        level:"Beginner and Advanced"
+     },
+     {
+        name:"Basic Course JavaScript",
+        price:290,
+        image:""  ,
+        duration:"2 days",
+        level:"Beginner"
+     },
+     {
+        name:"Basic Course HTML/CSS",
+        price:590,
+        image:""  ,
+        duration:"2 days",
+        level:"Beginner"
+     },
+     {
+        name:"Python Programming Course",
+        price:590,
+        image:""  ,
+        duration:"5 days",
+        level:"Beginner"
+     },
+]
